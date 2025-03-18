73cdc0f081b094e5f4a89b73a46befc8df3ffe4f
gulang9527 <zuzhaozhen@outlook.com>
2025-03-18 14:33:04 +0800

    Update __init__.py

--- managers/__init__.py
+++ managers/__init__.py
@@ -5,6 +5,7 @@ from managers.settings_manager import SettingsManager
 from managers.keyword_manager import KeywordManager
 from managers.stats_manager import StatsManager
 from managers.broadcast_manager import BroadcastManager
+from managers.recovery_manager import RecoveryManager 
 
 __all__ = [
     'SettingsManager',
